616059dafd2edf7278f276628e3122632a52603f
DOSOL\dosol <dosol48626@naver.com>
2025-02-26 16:07:05 +0900

    찐막 작업

--- src/pages/account/RegisterPage.js
+++ src/pages/account/RegisterPage.js
@@ -2,7 +2,9 @@ import  {register} from "../../services/accountService";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 //여기는 리코일 쓸게 없지. 회원가입이니까 ㅇㅇ
-//진짜 코파일럿 없이 코딩할까 현장에서 일하는 사람들은...
+
+import Landing from "../../components/ui/Landing";
+import styled from "styled-components";
 
 function RegisterPage() {
     const navigate = useNavigate();
@@ -23,37 +25,98 @@ function RegisterPage() {
     }
     //이거 콘솔에만 찍히게 할게 아니고 팝업이든 뭐든 띄어서 알려줘야겠다;;
     return(
-        <div>
+        <Container>
+            <LandingSection>
+                <Landing />
+            </LandingSection>
+            <RegisterSection>
             <h2>회원가입 페이지</h2>
-            <form onSubmit={handleRegister}>
-                <label>
-                    ID: <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    />
-                </label>
-                <label>
-                    PW: <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    />
-                </label>
-                <label>
-                    닉네임: <input
-                    type="text"
-                    value={nickname}
-                    onChange={(e) => setNickname(e.target.value)}
-                    />
-                </label>
-                <button type="submit">회원가입</button>
-            </form>
-            <p>
-                이미 계정이 있으면 로그인하러 가야지 <span onClick={() => navigate("/login")}>로그인</span>
-            </p>
-        </div>
+             <form onSubmit={handleRegister}>
+                 <label>
+                     ID: <input
+                     type="text"
+                     value={username}
+                     onChange={(e) => setUsername(e.target.value)}
+                     />
+                 </label>
+                 <label>
+                     PW: <input
+                     type="password"
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                     />
+                 </label>
+                 <label>
+                     닉네임: <input
+                     type="text"
+                     value={nickname}
+                     onChange={(e) => setNickname(e.target.value)}
+                     />
+                 </label>
+                 <button type="submit">회원가입</button>
+             </form>
+             <p>
+                 이미 계정이 있으면 로그인하러 가야지 <span onClick={() => navigate("/login")}>로그인</span>
+             </p>
+            </RegisterSection>
+        </Container>
+
+
+
+        
+        // <div>
+        //     <h2>회원가입 페이지</h2>
+        //     <form onSubmit={handleRegister}>
+        //         <label>
+        //             ID: <input
+        //             type="text"
+        //             value={username}
+        //             onChange={(e) => setUsername(e.target.value)}
+        //             />
+        //         </label>
+        //         <label>
+        //             PW: <input
+        //             type="password"
+        //             value={password}
+        //             onChange={(e) => setPassword(e.target.value)}
+        //             />
+        //         </label>
+        //         <label>
+        //             닉네임: <input
+        //             type="text"
+        //             value={nickname}
+        //             onChange={(e) => setNickname(e.target.value)}
+        //             />
+        //         </label>
+        //         <button type="submit">회원가입</button>
+        //     </form>
+        //     <p>
+        //         이미 계정이 있으면 로그인하러 가야지 <span onClick={() => navigate("/login")}>로그인</span>
+        //     </p>
+        // </div>
     );
 }
 
 export default RegisterPage;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+const LandingSection = styled.div`
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const RegisterSection = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`; 
